3edfa84c21b78ce5b1ba2ca8d2eb0fe0b67fe136
Moogamouth <moogamouth@mail.com>
2023-04-16 19:09:35 -0400

    Neatened up some code, added some bug fixes, optimizations and error messages.

--- index.js
+++ index.js
@@ -11,49 +11,61 @@ const arweave = require('arweave').init({
 });
 
 async function action(action, username, password, addr) {
-	await exec(`curl -v -d 'action=${action}' -k -u ${username}:${password} --anyauth --location ${addr}engine/job/Permaloom`);
+	return await exec(`curl -v -d 'action=${action}' -k -u ${username}:${password} --anyauth --location ${addr}engine/job/Permaloom`);
 }
 
 (async () => {
-	const warcDir = `${__dirname.substring(0, __dirname.lastIndexOf("/"))}/jobs/Permaloom/latest/warcs`;
-	
-	for (let warc of fs.readdirSync(warcDir)) {
-		fs.unlink(warc);
-	}
+	const warcPath = `${
+		__dirname
+		.substring(0, __dirname.lastIndexOf("/"))
+	}/jobs/Permaloom/latest/warcs`;
+
+	if (fs.existsSync(warcPath)) fs.rmSync(warcPath);
 
 	await exec(`$HERITRIX_HOME/bin/heritrix -a ${config.username}:${config.password}`);
 	
-	try {
-		exec(`curl -v -d "createpath=Permaloom&action=create" -k -u ${config.username}:${config.password} --anyauth --location \ ${config.addr}engine`);
-	}
-	catch {}
+	await exec(`curl -v -d "createpath=Permaloom&action=create" -k -u ${config.username}:${config.password} --anyauth --location \ ${config.addr}engine`);
 
 	await exec(`curl -v -T crawler-beans.cxml -k -u ${config.username}:${config.password} --anyauth --location ${config.addr}engine/job/Permaloom/jobdir/crawler-beans.cxml`);
 	
 	const actions = ['teardown', 'build', 'launch', 'unpause'];
 	for (i of actions) {
-		await action(i, config.username, config.password, config.addr);
+		if (i == 'build') {
+			output = 
+			(await action('build', config.username, config.password, config.addr))
+			.stdout
+			.split("<jobLogTail>")[1]
+			.split("</jobLogTail>")[0]
+			if (output.includes('SEVERE')) throw new Error(`Heritrix Error: \n${output.replace('<value>', '').replace('</value>', '')}`)
+			//replace isn't working here for some reason
+		}
+		else await action(i, config.username, config.password, config.addr);
 	}
 
 	console.log('Job started');
 
 	while (true) {
-		await new Promise(r => setTimeout(r, 1000));
-		if ((await exec(`curl -v -k -u ${config.username}:${config.password} --anyauth --location -H \"Accept: application/xml\" ${config.addr}engine/job/Permaloom`)).stdout.split("<crawlControllerState>")[1].split("</crawlControllerState>")[0] === 'FINISHED') {
+		crawlStatus = 
+		(await exec(`curl -v -k -u ${config.username}:${config.password} --anyauth --location -H \"Accept: application/xml\" ${config.addr}engine/job/Permaloom`))
+		.stdout
+		?.split("<crawlControllerState>")[1]
+		?.split("</crawlControllerState>")[0]
+		if (crawlStatus === 'FINISHED') {
 			let txs = [];
 			let fee = 0;
-			for (let warc of fs.readdirSync(`${__dirname.substring(0, __dirname.lastIndexOf("/"))}/jobs/Permaloom/latest/warcs`)) {
-				const tx = await arweave.createTransaction({data: fs.readFileSync(warcDir + warc)}, key);
-				txs += tx;
-				fee += tx.reward;
-			}
-			if (tx.reward < config.maxFee) {
-				for (let tx of txs) {
-					const uploader = await arweave.transactions.getUploader(await arweave.transactions.sign(tx, key));
-					while (!uploader.isComplete) await uploader.uploadChunk();
+			for (let warc of fs.readdirSync(warcPath)) {
+				if (fee < config.maxFee) {
+					const tx = await arweave.createTransaction({data: fs.readFileSync(warcPath + warc)}, key);
+					txs.push(tx);
+					fee += tx.reward;
 				}
+				fs.unlink(warc);
 			}
-			else throw new Error('Fee exceeded limit');
+			if (fee > config.maxFee) throw new Error('Fee exceeded limit');
+			for (let tx of txs) {
+				const uploader = await arweave.transactions.getUploader(await arweave.transactions.sign(tx, key));
+				while (!uploader.isComplete) await uploader.uploadChunk();
+			}
 			break;
 		}
 	}
